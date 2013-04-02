3e4c047128a6e9e04d73a124d1f6dfcf7f877b7c
sixiango <gm.zhouliang@gmail.com>
2013-04-02 23:41:21 +0800

    add common types definition

---
+++ com_types.h
@@ -0,0 +1,18 @@
+#ifndef __COM_TYPES_H__
+#define __COM_TYPES_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+typedef unsigned int u32;
+typedef int s32;
+typedef unsigned char u8;
+typedef unsigned short u16;
+
+#define iprintf printf
+/*
+#define bool int
+#define false 0
+#define true 1
+#define  inline 
+*/
+#endif //__COM_TYPES_H__
